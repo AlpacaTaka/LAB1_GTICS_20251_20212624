5f5612fa5d1442a33721806f5d29f677431297a2
ola <ola@ola.com>
2025-04-04 20:55:44 -0500

    base

---
+++ LAB1_GTICS_20251_20212624/src/main/java/com/example/lab1_gtics_20251_20212624/Lab1Gtics2025120212624Application.java
@@ -0,0 +1,13 @@
+package com.example.lab1_gtics_20251_20212624;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Gtics2025120212624Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1Gtics2025120212624Application.class, args);
+	}
+
+}
